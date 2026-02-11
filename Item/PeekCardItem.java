adda3727ec73643ead7923eed2ffccd15cb941fe
Natdanaii4 <dil182548@gmail.com>
2026-02-11 23:55:25 +0700

    oloo

--- Item/PeekCardItem.java
+++ Item/PeekCardItem.java
@@ -4,7 +4,7 @@ public class PeekCardItem extends Item {
     private String id = "CutCard";
 
     public void use(){
-        System.out.println(peekCurrent());
+        peekCurrent(); 
         ActionResult action = new ActionResult(false, null);
         action.continueTurn()
     }
